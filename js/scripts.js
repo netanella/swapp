122d70285c2d9aac6bf66e835affb1c4be96e701
Netanella <netanella1@gmail.com>
2019-11-17 19:00:49 +0200

    login modals

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval: 4000});
+    $('#carouselButton').click(function(){
+        if($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+
+    });
+    $('#carousel-play').click(function(){
+        $('#mycarousel').carousel('cycle');
+    });
+
+    /**Modal popups**/
+
+    $('.loginButton').click(function(){
+        $('#loginModal').modal('show');
+    });
+    $('.signupButton').click(function(){
+        $('#signupModal').modal('show');
+    });
+
+});
+
+
